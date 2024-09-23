34c7851a73921db23e9fd5104b89921282d069c4
ibrahim muhammad <ibrex29@gmail.com>
2024-09-23 18:25:55 +0100

    update

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import * as compression from 'compression';
+
+const corsOptions: CorsOptions = {
+  origin: true,
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'], // Allow specific HTTP methods
+  allowedHeaders: ['*'], // Allow specific headers
+};
+
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
 
+  const app = await NestFactory.create(AppModule);
+  app.use(compression()); // use compression to improve speed
+  app.enableCors(corsOptions);
+  app.setGlobalPrefix('api');
   const config = new DocumentBuilder()
     .setTitle('Hope API')
     .setDescription('The Hope API description')
