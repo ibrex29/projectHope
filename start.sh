39dc5b182eeae62e89242ff9295b80124f71ddcd
Sadiq Umar <umarsadiqibrahim@gmail.com>
2025-01-20 05:19:06 +0100

    modfiy dockerfile

--- start.sh
+++ start.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# Ensure DATABASE_URL is set
+if [ -z "$DATABASE_URL" ]; then
+  echo "Error: DATABASE_URL is not set."
+  exit 1
+fi
+
 # Run Prisma migration at runtime
 npx prisma migrate deploy
 
